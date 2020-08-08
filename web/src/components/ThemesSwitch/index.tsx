c9e8f63cdf82d718dae130ea0f2de9ab21c651f7
Vinicius Pascoalini <65316815+ViniciusPascoalini@users.noreply.github.com>
2020-08-08 11:57:07 -0300

    Icons on Themes Switch

--- web/src/components/ThemesSwitch/index.tsx
+++ web/src/components/ThemesSwitch/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import { FiMoon, FiSun } from 'react-icons/fi'
 
 import './styles.css';
 
 function ThemesSwitch(){
     return (
             <div className="switch_container">
+
+                <div className="sun">
+                    
+                    <FiSun />
+                            
+                </div>
+
                 <input id="switch_shadow" className="switch switch_shadow"  type="checkbox"/>
                 <label htmlFor="switch_shadow"></label>
+
+                <div className="moom">
+
+                    <FiMoon />
+
+                </div>
+
             </div>
        
     );
